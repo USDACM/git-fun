8f1010d5c8b92bc3c1d98ad28f95c3250b047e73
Nicholas Otto <nicholas.d.otto@gmail.com>
2013-11-18 00:25:02 -0800

    Added some more changes

---
+++ tmp/Hello.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Hello{
+
+    public static void main(String[] args){
+        System.out.printf("Hello ACM club!\n");
+        
+        /*Change1*/
+        printPlan();
+        System.exit(0);
+    }
+
+    static void printPlan(){
+        System.out.printf("\t1. Greetings\n");
+        System.out.printf("\t2. Competition Thoughts\n");
+        System.out.printf("\t3. Coffee Pot\n");
+        System.out.printf("\t4. Movie Night\n");
+        System.out.printf("\t5. uhub\n");
+        System.out.printf("\t6. git tutorial\n");
+    };
+}

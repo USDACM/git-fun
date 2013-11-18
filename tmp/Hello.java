d878c7e6f14e4ac633fffd7688e466a3286e4b87
Nicholas Otto <nicholas.d.otto@gmail.com>
2013-11-18 00:26:30 -0800

    changed the order of the day

--- tmp/Hello.java
+++ tmp/Hello.java
@@ -9,12 +9,13 @@ class Hello{
         System.exit(0);
     }
 
+    /*Change1*/
     static void printPlan(){
         System.out.printf("\t1. Greetings\n");
         System.out.printf("\t2. Competition Thoughts\n");
         System.out.printf("\t3. Coffee Pot\n");
         System.out.printf("\t4. Movie Night\n");
-        System.out.printf("\t5. uhub\n");
         System.out.printf("\t6. git tutorial\n");
+        System.out.printf("\t5. uhub\n");
     };
 }

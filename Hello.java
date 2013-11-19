efc2317df08afc36e5ef09aee709ac9eece0918b
Nicholas Otto <nicholas.d.otto@gmail.com>
2013-11-19 12:50:58 -0800

    Hello addition

--- Hello.java
+++ Hello.java
@@ -3,6 +3,20 @@ class Hello{
 
     public static void main(String[] args){
         System.out.printf("Hello ACM club!\n");
+
+        printPlan();
         System.exit(0);
     }
+
+
+    /*Change1*/
+    static void printPlan(){
+        System.out.printf("\t1. Greetings\n");
+        System.out.printf("\t2. Competition Thoughts\n");
+        System.out.printf("\t3. Coffee Pot\n");
+        System.out.printf("\t4. Movie Night\n");
+        System.out.printf("\t5. uhub\n");
+        System.out.printf("\t6. Git tutorial\n");
+    };
+
 }
